bff166750199d087664a3d69a70519da000c5dee
Kevin Figuera <kevinalejandro3@gmail.com>
2021-06-11 11:03:13 -0400

    Configuración Inicial

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Note;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()->create([
+            'email' => 'kfiguera@smartwebtools.net',
+            'password' => bcrypt('123456')
+        ]);
+        Note::factory(30)->create();
+    }
+}
